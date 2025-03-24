6d5db3913b3b1961a46b06364faefb7c9ce49aa9
notevenalex <dylanalex23@gmail.com>
2025-03-24 10:54:13 -0500

    Entitys of ot, i forgot to save 😁😁

--- src/order-&-transactions/entities/orderTrans.entity.ts
+++ src/order-&-transactions/entities/orderTrans.entity.ts
@@ -1,2 +1,28 @@
+import { MakeUpProduct } from "src/makeup-products/entities/makeup-product.entity";
+import { Entity, PrimaryGeneratedColumn, Column, Collection} from "typeorm";
 
-export class OrderTrans {}
+export enum PaymentStat {
+    PAID = 'paid',
+    REFUNDED = 'refunded',
+    FAILED = 'failed'
+}
+
+@Entity('orderTrans')
+export class OrderTrans {
+
+@PrimaryGeneratedColumn('uuid')
+id : string;
+
+@Column('uuid')
+client_id : string
+
+@Column()
+products : MakeUpProduct
+
+@Column('decimal')
+total_amount : number
+
+@Column({ type: 'enum', enum: PaymentStat })
+payment_status : PaymentStat
+
+}
