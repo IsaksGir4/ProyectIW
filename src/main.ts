f8a103119c668f7271e181e75b2744155b641354
notevenalex <dylanalex23@gmail.com>
2025-05-28 08:41:14 -0500

    back

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,14 @@ console.log('DATABASE_URL desde dotenv:', process.env.DATABASE_URL); // 🔍 Dep
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+// Habilitar CORS
+  app.enableCors({
+    origin: 'http://localhost:5173', // Permite solicitudes solo desde tu frontend React
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Permite los métodos HTTP necesarios
+    credentials: true, // Si usas cookies, encabezados de autorización, etc.
+  });
+
+  await app.listen(3000); // Asegúrate de que el puerto sea el mismo que el frontend espera
 }
 bootstrap();
 
